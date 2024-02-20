7e971bdc25da7666f42816e073a243b7d3ce44c3
Gpriya23 <145136421+Gpriya23@users.noreply.github.com>
2024-02-20 09:26:01 +0530

    Create largest number.py

---
+++ largest number.py
@@ -0,0 +1,19 @@
+#print largest number
+'''a=12
+b=13
+if a>b:
+    print(a)
+else:
+    print(b)'''
+
+
+
+'''a = int(input())
+b = int(input())
+c = int(input())
+if a>b and a>c:
+    print(a)
+elif b>a and b>c:
+        print(b)
+else:
+    print(C)'''
